eafe2aa72a3607c6750fe2c0941aaa2d873591b6
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2025-04-27 16:23:58 +0500

    markup safe and using of variables

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
  
 
 from flask import Flask
+# This will protect us from injections 
+from markupsafe import escape
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,7 +13,17 @@ def base():
 
 @app.route('/home')
 def Home():
-    return "<h1>I am Home page<h1>"
+    return "<h1>I am Home page</h1>"
+
+@app.route('/<name>')
+def NamePage(name):
+    return f"<h1>Hello , {escape(name)}</h1> "
+
+@app.route('/id/<int:post_id>')
+def IdPage(post_id):
+    return f"Hello , {escape(post_id)} "
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
